3e0179bfc741cad18fd5db776b413160f9761e7a
Leonnardo Lair T. Gomes <leonnardo0316@gmail.com>
2025-07-10 02:45:40 -0300

    home fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,10 @@ class Home extends React.Component {
     <h1 id="inicio">Leonnardo Lair Tavares Gomes</h1>
     <img src={leo} id="leoimg" alt="Imagem de Perfil"/>
 
-    <p>Cursando o <strong>8° Periodo de Engenharia da computação</strong> e <strong>Estudante da Trybe.</strong> 
-        Também gosta de se distrair com jogos coloridos </p>
+    <p>Cursando o <strong>3° Periodo de Sistema de Informação</strong> e 
+        Também gosta de se distrair com jogos coloridos e jogar um RPGzin </p>
 
-    <p><i>~Ser Persistente e ser Cabeça dura são dois lados da mesma moeda!</i></p>
+    <p><i>~I'm trying to exist</i></p>
 
     <a target="" href="https://github.com/Noarito0316">Perfil do Github </a>
 
